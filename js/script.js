223ae0d0bf75e2190084aadd4accc4d5c86c1cdb
muammar yasir <132113432+itsmarmot@users.noreply.github.com>
2024-08-07 09:26:04 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin : 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-  strings: ['Programmer', 'Frontend Developer', 'Data Scientist'],
+  strings: ['Programmer', 'Frontend Developer', 'ML Engineer'],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
